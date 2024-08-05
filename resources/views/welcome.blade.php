07311187e2d5160ef1f3f910748b0344ae489323
Ayham Ibrahim <ayhamfacebook1999@gmail.com>
2024-08-06 00:47:31 +0300

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,168 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html>
+<head>
+	<title>Ayham Ibrahim | Web Developer</title>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <meta name="keywords" content="" />
+	<meta name="description" content="" />
+	<link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/2.7.0/build/reset-fonts-grids/reset-fonts-grids.css" media="all" />
+	<link rel="stylesheet" type="text/css" href="{{ asset('css/resume.css') }}" media="all" />
+</head>
+<body>
+
+<div id="doc2" class="yui-t7">
+	<div id="inner">
+
+		<div id="hd">
+			<div class="yui-gc">
+				<div class="yui-u first">
+					<h1>Ayham Ibrahim</h1>
+					<h2>Web Developer,Laravel</h2>
+				</div>
+
+				<div class="yui-u">
+					<div class="contact-info">
+						<h3><a id="pdf" href="#">Download PDF</a></h3>
+						<h3><a href="mailto:ayhamib99@gmail.com">ayhamib99@gmail.com</a></h3>
+						<h3>(+963) 943578325</h3>
+					</div><!--// .contact-info -->
+				</div>
+			</div><!--// .yui-gc -->
+		</div><!--// hd -->
+
+		<div id="bd">
+			<div id="yui-main">
+				<div class="yui-b">
+
+					<div class="yui-gf">
+						<div class="yui-u first">
+							<h2>SUMMARY</h2>
+						</div>
+						<div class="yui-u">
+							<p class="enlarge">
+                                A back-end developer with good experience in the PHP language and the Laravel
+                                framework . Have good problem-solving skills. Completed a set of projects that meet
+                                user needs and I am ready to deliver more projects and learn new skills all the time.
+                                committed to keeping up with the latest website development technologies and skills
+                            </p>
+						</div>
+					</div><!--// .yui-gf -->
+
+					<div class="yui-gf">
+						<div class="yui-u first">
+							<h2>Skills</h2>
+						</div>
+						<div class="yui-u">
+                            <ul class="talent">
+                                <li>Laravel</li>
+                                <li>PHP</li>
+                                <li class="last">MySQL</li>
+                            </ul>
+							<ul class="talent">
+								<li>RESTful API</li>
+								<li>HTML</li>
+								<li class="last">CSS</li>
+							</ul>
+
+							<ul class="talent">
+								<li>JavaScript</li>
+								<li>Bootstrap</li>
+								<li class="last">React.js</li>
+							</ul>
+							<ul class="talent">
+								<li>Git & GitHub</li>
+								<li>Trello</li>
+								<li class="last">Communication skills</li>
+							</ul>
+
+						</div>
+					</div><!--// .yui-gf-->
+
+					<div class="yui-gf">
+
+						<div class="yui-u first">
+							<h2>Experience</h2>
+						</div><!--// .yui-u -->
+
+						<div class="yui-u">
+
+							<div class="job">
+								<h2>Focal X Company</h2>
+								<h3>Supervisor in training team (Backend Web Development)</h3>
+								<h4> February 2024 – July 2024 (Remotely)</h4>
+								<p style="font-size: 15px;padding-left:20px;line-height:1.4rem">Managed and provided guidance to students as they completed assignments and
+                                    projects. I actively engaged with students, answered questions, and provided feedback
+                                    to help them. Improve their skills in Backend Web Development. </p>
+							</div>
+
+							<div class="job">
+								<h2>Focal X Agency</h2>
+								<h3>Full Stack web Development training</h3>
+								<h4>February 2024 – July 2024</h4>
+                                <ul class="job-details">
+                                    <li>Working on developing web applications using different frameworks by
+                                        employing Laravel, ReactJS, TypeScript, and MySQL to create web applications
+                                        with specifications that satisfy the user.
+                                    </li>
+                                    <li>Working within a team to develop websites to enhance teamwork skills and
+                                        contribute to helping each other develop our skills
+                                    </li>
+                                    <li>Developing individual skills to write clean and highly efficient codes in order to
+                                        obtain high-performance results and achieve the best consumption of resources
+                                    </li>
+                                </ul>
+							</div>
+
+							<div class="job">
+								<h2>Focal X Agency</h2>
+								<h3>Backend Web Development</h3>
+								<h4>Jul 2023 - January 2024</h4>
+                                <ul class="job-details">
+                                    <li>
+                                        Completing server-side projects using the Laravel framework for mobile applications and websites
+                                    </li>
+                                    <li>
+                                        I worked on completing many tasks using Laravel framework with a team within the company after completing the training
+                                    </li>
+                                    <li>
+                                        In addition to completing individual tasks during the training period using PHP programming language and Laravel framework
+                                    </li>
+                                </ul>
+							</div>
+
+						</div><!--// .yui-u -->
+					</div><!--// .yui-gf -->
+
+
+					<div class="yui-gf last">
+						<div class="yui-u first">
+							<h2>Education</h2>
+						</div>
+						<div class="yui-u job">
+							<h2>Al-Baath university | present</h2>
+							<ul class="job-details"><li>studying a master's degree in computer networks</li></ul>
+						</div>
+						<div class="yui-u job">
+							<h2>Al-Baath university | 2018 – 2022</h2>
+							<ul class="job-details"><li>Studied network and information systems engineering at the College of Informatics Engineering</li></ul>
+						</div>
+					</div><!--// .yui-gf -->
+
+
+				</div><!--// .yui-b -->
+			</div><!--// yui-main -->
+		</div><!--// bd -->
+
+		<div id="ft">
+			<p>Ayham Ibrahim &mdash; <a href="mailto:ayhamib99@gmail.com">ayhamib99@gmail.com</a> &mdash; (+963) 943578325</p>
+		</div><!--// footer -->
+
+	</div><!-- // inner -->
+
+
+</div><!--// doc -->
+
+
+</body>
+</html>
+
